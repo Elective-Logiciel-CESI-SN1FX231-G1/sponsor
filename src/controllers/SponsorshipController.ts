17b6c70303c90cdb9dc1e774ccf0513912ac712a
TomBANCHEREAU <tom.banchereau@viacesi.fr>
2022-06-20 08:39:53 +0200

    add pagination

--- src/controllers/SponsorshipController.ts
+++ src/controllers/SponsorshipController.ts
@@ -2,8 +2,15 @@ import { Handler } from 'express'
 import SponsorshipModel from '../models/SponsorshipModel'
 
 export const getAll: Handler = async (req, res) => {
-  const Sponsorships = await SponsorshipModel.find({ sponsor: { _id: req.user?._id } })
-  res.send(Sponsorships)
+  const query = { 'sponsor._id': req.user?._id }
+  const [results, count] = await Promise.all([
+    SponsorshipModel.find(query).skip(req.pagination.skip).limit(req.pagination.size).exec(),
+    SponsorshipModel.countDocuments(query).exec()
+  ])
+  res.send({
+    count,
+    results
+  })
 }
 
 export default {
--- src/routes/SponsorshipRouter.ts
+++ src/routes/SponsorshipRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { authNeeded } from '../auth'
 import SponsorshipController from '../controllers/SponsorshipController'
+import paginate from '../utils/pagination'
 const SponsorshipRouter = express.Router()
 
-SponsorshipRouter.get('/', authNeeded, SponsorshipController.getAll)
+SponsorshipRouter.get('/', authNeeded, paginate, SponsorshipController.getAll)
 
 export default SponsorshipRouter
